4a8de64ca3188cd1a78129355e1753904b418fcf
Joseph Novakovich <josephnovakovich@gmail.com>
2026-03-19 13:22:16 -0400

    minorly

--- C/154.c
+++ C/154.c
@@ -11,9 +11,10 @@ I v_p(I p,I n) { I x = 0; for (I pk=p; pk<=n; pk*=p) x += n/pk; R x; }
 I main () {
   I a = 0; for (I k=1; k <= N; k++) { V2[k] = v_p(2,k); V5[k] = v_p(5,k); }
   
-  for (I i=0; i <= N/3; i++) {
-    for (I j = i; j <= (N-i)/2; j++) { I k = N-i-j;
-      if (12 <= V2[N]-V2[i]-V2[j]-V2[k] && 12 <= V5[N]-V5[i]-V5[j]-V5[k]) {
-	a += S[(i!=j)+(j!=k)]; } } }
+  for (I i=0; i <= N/3; i++)
+    for (I j = i; j <= (N-i)/2; j++) {
+      I k = N-i-j;
+      if (12 <= V2[N]-V2[i]-V2[j]-V2[k] && 12 <= V5[N]-V5[i]-V5[j]-V5[k])
+	a += S[(i!=j)+(j!=k)]; }
 
   printf("%llu\n",a); R 0; }
