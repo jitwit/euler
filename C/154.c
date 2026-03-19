2851ec142841fedc21290c85167489e1aec5cd82
Joseph Novakovich <josephnovakovich@gmail.com>
2026-03-19 13:13:52 -0400

    minorly

--- C/154.c
+++ C/154.c
@@ -3,20 +3,17 @@ typedef unsigned long long I;
 #define N 200000
 #define R return
 
-I V2[N+1] = {0};I V5[N+1] = {0};
+I V2[N+1], V5[N+1], S[3]={1,3,6};
 
 // legendre p-adic valuation (how many times p divides n!)
 I v_p(I p,I n) { I x = 0; for (I pk=p; pk<=n; pk*=p) x += n/pk; R x; }
 
 I main () {
-  I ans = 0; for (I k=1; k <= N; k++) { V2[k] = v_p(2,k); V5[k] = v_p(5,k); }
+  I a = 0; for (I k=1; k <= N; k++) { V2[k] = v_p(2,k); V5[k] = v_p(5,k); }
   
   for (I i=0; i <= N/3; i++) {
     for (I j = i; j <= (N-i)/2; j++) { I k = N-i-j;
       if (12 <= V2[N]-V2[i]-V2[j]-V2[k] && 12 <= V5[N]-V5[i]-V5[j]-V5[k]) {
-	if      (i != j && j != k) ans += 6;
-	else if (i != j || j != k) ans += 3;
-	else                       ans += 1; } } }
+	a += S[(i!=j)+(j!=k)]; } } }
 
-  printf("%lld\n",ans);
-  return 1; }
+  printf("%llu\n",a); R 0; }
